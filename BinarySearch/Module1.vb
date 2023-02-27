979fe4912131a6adf3bdfb96183b977a0b9a5204
Aaron Rodrigues <38214417+aaronjrodrigues@users.noreply.github.com>
2023-02-27 19:20:38 +0200

    Add files via upload

---
+++ BinarySearch/Module1.vb
@@ -0,0 +1,35 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim Array() As Integer = {0, 2, 2, 3, 4, 8, 25, 27, 35, 47}
+        Dim Query, Half, FoundPos As Integer
+        Dim Found As Boolean
+
+        Found = False
+
+        Console.WriteLine("Enter search query")
+        Query = Console.ReadLine
+
+        Half = 5
+
+        Do
+            If Array(Half) > Half Then
+                Half = CInt(Half / 2) + Half
+
+            End If
+
+            If Array(Half) = Query Then
+                Found = True
+                FoundPos = Half
+            End If
+
+            If Array(Half) < Query Then
+                Half = Half - CInt(Half / 2)
+            End If
+
+        Loop Until Found = True Or Half = 1 Or Half = 9
+
+
+    End Sub
+
+End Module
